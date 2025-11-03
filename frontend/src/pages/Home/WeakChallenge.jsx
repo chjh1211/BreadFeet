3a28b16b92af6594614db712c1d4473a908074b1
jun <jueun1827@naver.com>
2025-11-04 04:44:20 +0900

    feat: EventDetailPage 생성

--- frontend/src/pages/Home/WeakChallenge.jsx
+++ frontend/src/pages/Home/WeakChallenge.jsx
@@ -1,13 +1,13 @@
 import EventCard from "../../components/EventCard";
 
 const WeakChallenge = () => {
-   
 
     return (
         <div className="">
         <h2 className="">이번주 Top 3 챌린지</h2>
         <div className="ChallengeList">
            <EventCard 
+           type="recommended"
             url="http://localhost:3001/recommended" // 추천 스티커 API
             />
         </div>
