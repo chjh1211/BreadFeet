ce2586a8711366ae79f51944bbcb8ddd81be1d01
JamL <691285@naver.com>
2025-11-06 17:17:16 +0900

    init: setup base repository structure

--- backend/src/main/java/com/breadfeet/login/BreadfeetApplication.java
+++ backend/src/main/java/com/breadfeet/login/BreadfeetApplication.java
@@ -1,4 +1,4 @@
-package com.breadfeet.backend;
+package com.breadfeet.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
