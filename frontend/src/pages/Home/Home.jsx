b355c2ef5b7231183ada538c15d82ad831da67dd
gamza <aprille138@gmail.com>
2025-11-10 14:15:49 +0900

    feat: home login CTA component 1

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import "./Home.css";
 import WeakChallenge from "./WeakChallenge";
 import CTAnoLogin from "./CTAnoLogin";
 import TopBakeries from "./TopBakeries";
+import BestCourseCTA from "./bestCourseCTA";
 import { Row, Col } from "react-bootstrap";
 
 const Home = () => {
@@ -13,7 +14,8 @@ const Home = () => {
       <Row className="m-4 justify-content-center">
         <Col md={6} className="m-3">
           {/* <TopBakery /> */}
-          <CTAnoLogin />
+          {/* <CTAnoLogin /> */}
+          <BestCourseCTA />
         </Col>
         <Col md={4} className="m-3">
           <TopBakeries />
