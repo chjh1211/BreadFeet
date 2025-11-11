0e415e062703af0819b1ea069b84f6823d8d22ad
chjh1211 <chjh8800@naver.com>
2025-11-11 21:42:35 +0900

    [Fix] 즐겨찾기 api 경로 수정

--- breadfeet_BE/src/main/java/com/example/breadfeet_BE/domain/favorite/FavoriteController.java
+++ breadfeet_BE/src/main/java/com/example/breadfeet_BE/domain/favorite/FavoriteController.java
@@ -1,6 +1,5 @@
 package com.example.breadfeet_BE.domain.favorite;
 
-import com.example.breadfeet_BE.domain.favorite.dto.FavoriteRequestDto;
 import com.example.breadfeet_BE.domain.favorite.dto.FavoriteResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,25 +12,25 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users/me/favorites")
+@RequestMapping("/api/v1/bakeries")
 public class FavoriteController {
 
     private final FavoriteService favoriteService;
 
     // 즐겨찾기 추가
-    @PostMapping
-    public ResponseEntity<Map<String,String>> addFavorite(@RequestBody FavoriteRequestDto favoriteRequestDto) {
+    @PostMapping("/{bakeryId}/favorite")
+    public ResponseEntity<Map<String,String>> addFavorite(@PathVariable Long bakeryId) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Long userId = Long.parseLong(authentication.getName());
 
-        FavoriteResponseDto favoriteResponseDto = favoriteService.addFavorite(userId, favoriteRequestDto.getBakeryId());
+        FavoriteResponseDto favoriteResponseDto = favoriteService.addFavorite(userId, bakeryId);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(Map.of("message", "추가 완료"));
     }
 
     // 즐겨찾기 삭제
-    @DeleteMapping("/{bakeryId}")
+    @DeleteMapping("/{bakeryId}/favorite")
     public ResponseEntity<Void> deleteFavorite(@PathVariable Long bakeryId) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Long userId = Long.parseLong(authentication.getName());
